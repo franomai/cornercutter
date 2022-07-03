89f5d0fc6ad5f119430210603100d7455cc31665
pumbas600 <joshjeffers600@gmail.com>
2022-07-03 22:57:12 +1200

    #3 Created example Tauri command

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,16 @@ import './App.css';
 import { Button, Checkbox, Container, Stack } from '@chakra-ui/react';
 import Configuration from './types/Configuration';
 import { invoke } from '@tauri-apps/api';
+import { convertKeysToSnakeCase } from './utility/Utils';
+
+const EMPTY_CONFIG: Configuration = {
+    disableHp: false,
+    disableOtherDrops: false,
+};
 
 function App() {
     const [response, setResponse] = useState<string>('');
-    const [config, setConfig] = useState<Configuration>({});
+    const [config, setConfig] = useState<Configuration>(EMPTY_CONFIG);
 
     useEffect(() => {
         console.log(config);
@@ -21,8 +27,7 @@ function App() {
     }
 
     async function submitConfig() {
-        setConfig({});
-        const res = await invoke<string>('submitConfig', { config: config });
+        const res = await invoke<string>('accept_config', { config: convertKeysToSnakeCase(config) });
         setResponse(res);
     }
 
@@ -30,11 +35,11 @@ function App() {
         <div className="App">
             <Container>
                 <Stack spacing={5} my={4}>
-                    <Checkbox isChecked={config.disableHp === true} onChange={(e) => updateConfig('disableHp', e)}>
+                    <Checkbox isChecked={config.disableHp} onChange={(e) => updateConfig('disableHp', e)}>
                         Disable HP
                     </Checkbox>
                     <Checkbox
-                        isChecked={config.disableOtherDrops === true}
+                        isChecked={config.disableOtherDrops}
                         onChange={(e) => updateConfig('disableOtherDrops', e)}
                     >
                         Disable other drops
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,24 @@
   windows_subsystem = "windows"
 )]
 
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Configuration  {
+    disable_hp: bool,
+    disable_other_drops: bool
+}
+
+#[tauri::command]
+fn accept_config(config: Configuration) -> String {
+  return format!("You submitted: Disable HP: {}, Disable other drops: {}", config.disable_hp, config.disable_other_drops);
+}
+
 fn main() {
   let context = tauri::generate_context!();
   tauri::Builder::default()
     .menu(tauri::Menu::os_default(&context.package_info().name))
+    .invoke_handler(tauri::generate_handler![accept_config])
     .run(context)
     .expect("error while running tauri application");
 }
