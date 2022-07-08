fd046342c4aa957abbbbf3589886a7dec6bd0d89
pumbas600 <joshjeffers600@gmail.com>
2022-07-08 13:54:07 +1200

    #3 Added general configuration options and moved it to separate component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,55 +1,8 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { Button, Checkbox, Container, Stack } from '@chakra-ui/react';
-import Configuration from './types/Configuration';
-import { invoke } from '@tauri-apps/api';
-import { convertKeysToSnakeCase } from './utility/Utils';
-
-const EMPTY_CONFIG: Configuration = {
-    disableHp: false,
-    disableOtherDrops: false,
-};
+import GeneralConfig from './pages/GeneralConfig';
 
 function App() {
-    const [response, setResponse] = useState<string>('');
-    const [config, setConfig] = useState<Configuration>(EMPTY_CONFIG);
-
-    useEffect(() => {
-        console.log(config);
-    }, [config]);
-
-    function updateConfig(field: keyof Configuration, e: React.ChangeEvent<HTMLInputElement>) {
-        setConfig((config) => {
-            const updatedConfig = { ...config };
-            updatedConfig[field] = e.target.checked;
-            return updatedConfig;
-        });
-    }
-
-    async function submitConfig() {
-        const res = await invoke<string>('accept_config', { config: convertKeysToSnakeCase(config) });
-        setResponse(res);
-    }
-
-    return (
-        <div className="App">
-            <Container>
-                <Stack spacing={5} my={4}>
-                    <Checkbox isChecked={config.disableHp} onChange={(e) => updateConfig('disableHp', e)}>
-                        Disable HP
-                    </Checkbox>
-                    <Checkbox
-                        isChecked={config.disableOtherDrops}
-                        onChange={(e) => updateConfig('disableOtherDrops', e)}
-                    >
-                        Disable other drops
-                    </Checkbox>
-                    <Button onClick={submitConfig}>Test sending to Tauri Core!</Button>
-                    {response.length !== 0 && <div>{response}</div>}
-                </Stack>
-            </Container>
-        </div>
-    );
+    return <GeneralConfig />;
 }
 
 export default App;
---
+++ frontend/src/pages/GeneralConfig.tsx
@@ -0,0 +1,84 @@
+import { FC, useState } from 'react';
+import { Button, Checkbox, Container, Radio, RadioGroup, Stack, Text } from '@chakra-ui/react';
+import Configuration from '../types/Configuration';
+import { invoke } from '@tauri-apps/api';
+import { convertKeysToSnakeCase } from '../utility/Utils';
+
+const EMPTY_CONFIG: Configuration = {
+    disableHp: false,
+    disableOtherDrops: false,
+};
+
+const GeneralConfig: FC = () => {
+    const [response, setResponse] = useState<string>('');
+    const [config, setConfig] = useState<Configuration>(EMPTY_CONFIG);
+
+    function updateConfig(field: keyof Configuration, e: React.ChangeEvent<HTMLInputElement>) {
+        setConfig((config) => {
+            const updatedConfig = { ...config };
+            updatedConfig[field] = e.target.checked;
+            return updatedConfig;
+        });
+    }
+
+    async function submitConfig() {
+        const res = await invoke<string>('accept_config', { config: convertKeysToSnakeCase(config) });
+        setResponse(res);
+    }
+
+    return (
+        <div className="App">
+            <Container my={4}>
+                <Stack spacing={4}>
+                    <div>
+                        <Text fontSize="lg">Spawns</Text>
+                        <RadioGroup defaultValue="1">
+                            <Stack direction="row" spacing={4}>
+                                <Radio value="1">Looped</Radio>
+                                <Radio value="2">Weighted</Radio>
+                            </Stack>
+                        </RadioGroup>
+                    </div>
+                    <div>
+                        <Text fontSize="lg">Curse spawns</Text>
+                        <RadioGroup defaultValue="1">
+                            <Stack direction="row" spacing={4}>
+                                <Radio value="1">Random</Radio>
+                                <Radio value="2">Always</Radio>
+                                <Radio value="3">Never</Radio>
+                            </Stack>
+                        </RadioGroup>
+                    </div>
+                    <div>
+                        <Text fontSize="lg">Configure per</Text>
+                        <Stack direction="row" spacing={4}>
+                            <Checkbox>Floor</Checkbox>
+                            <Checkbox>Room</Checkbox>
+                        </Stack>
+                    </div>
+                    <Stack spacing={1}>
+                        <Text fontSize="lg">Other options</Text>
+                        <Checkbox isChecked={config.disableHp} onChange={(e) => updateConfig('disableHp', e)}>
+                            Disable HP
+                        </Checkbox>
+                        <Checkbox
+                            isChecked={config.disableOtherDrops}
+                            onChange={(e) => updateConfig('disableOtherDrops', e)}
+                        >
+                            Disable other drops
+                        </Checkbox>
+                        <Checkbox>Blank on empty</Checkbox>
+                        <Checkbox>Disable pinned</Checkbox>
+                        <Checkbox>Award per level</Checkbox>
+                    </Stack>
+                </Stack>
+                <Stack spacing={4}>
+                    <Button onClick={submitConfig}>Test sending to Tauri Core!</Button>
+                    {response.length !== 0 && <div>{response}</div>}
+                </Stack>
+            </Container>
+        </div>
+    );
+};
+
+export default GeneralConfig;
