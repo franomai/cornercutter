d49f686de7271df29043a685fb4bb12ed3b1b88e
pumbas600 <joshjeffers600@gmail.com>
2022-07-08 14:29:17 +1200

    #3 Created config context provider

--- frontend/src/types/Configuration.ts
+++ frontend/src/types/Configuration.ts
@@ -2,3 +2,8 @@ export default interface Configuration {
     disableHp: boolean;
     disableOtherDrops: boolean;
 }
+
+export const DEFAULT_CONFIG: Configuration = {
+    disableHp: false,
+    disableOtherDrops: false,
+};
