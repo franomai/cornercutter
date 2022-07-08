aefda7d164b06af039c21d21868ccb6314a333f5
pumbas600 <joshjeffers600@gmail.com>
2022-07-08 14:35:01 +1200

    #3 Replaced interface with tuple type

--- frontend/src/context/ConfigContext.tsx
+++ frontend/src/context/ConfigContext.tsx
@@ -1,33 +1,24 @@
 import { createContext, FC, ReactNode, useContext, useState } from 'react';
 import Configuration, { DEFAULT_CONFIG } from '../types/Configuration';
 
-interface ConfigContextProps {
-    config: Configuration;
-    setConfig: React.Dispatch<React.SetStateAction<Configuration>>;
-}
+type ConfigContextValue = [Configuration, React.Dispatch<React.SetStateAction<Configuration>>];
 
 interface Props {
     children?: ReactNode;
 }
 
-const ConfigContext = createContext<ConfigContextProps>({
-    config: DEFAULT_CONFIG,
-    setConfig: () => {},
-});
+const ConfigContext = createContext<ConfigContextValue>([DEFAULT_CONFIG, () => {}]);
 
 const useConfigContext = () => useContext(ConfigContext);
 
 const ConfigContextProvider: FC<Props> = (props) => {
     const [config, setConfig] = useState<Configuration>(DEFAULT_CONFIG);
 
-    const contextValue: ConfigContextProps = {
-        config,
-        setConfig,
-    };
+    const contextValue: ConfigContextValue = [config, setConfig];
 
     return <ConfigContext.Provider value={contextValue}>{props.children}</ConfigContext.Provider>;
 };
 
 export default useConfigContext;
 export { ConfigContextProvider };
-export type { ConfigContextProps };
+export type { ConfigContextValue };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,16 @@ import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ConfigContextProvider } from './context/ConfigContext';
 import './index.css';
 import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ChakraProvider theme={theme}>
-            <App />
+            <ConfigContextProvider>
+                <App />
+            </ConfigContextProvider>
         </ChakraProvider>
     </React.StrictMode>,
 );
