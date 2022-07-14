6a236a6377c2bd6438a9c24c31239126ad08611f
pumbas600 <joshjeffers600@gmail.com>
2022-07-14 14:18:37 +1200

    #3 Created search and filtering

--- frontend/src/components/SearchColumn.tsx
+++ frontend/src/components/SearchColumn.tsx
@@ -1,5 +1,5 @@
-import { Box, Input, Stack, Text } from '@chakra-ui/react';
-import { FC, ReactNode } from 'react';
+import { Box, Divider, Input, Stack, Text } from '@chakra-ui/react';
+import { FC, ReactNode, useEffect, useState } from 'react';
 import SearchColumnItem from '../types/SearchColumnItem';
 
 interface Props {
@@ -7,25 +7,45 @@ interface Props {
 }
 
 const SearchColumn: FC<Props> = (props) => {
+    const [search, setSearch] = useState('');
+    const [filteredResults, setFilteredResults] = useState(props.items);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            const loweredSearch = search.toLowerCase();
+            setFilteredResults(props.items.filter((item) => item.name.toLowerCase().includes(loweredSearch)));
+        }, 500); // Wait 0.5 seconds after they've finished typing to apply the filter
+
+        return () => clearTimeout(timeout);
+    }, [search]);
+
     function renderItems(): ReactNode {
-        if (props.items.length === 0) {
+        if (filteredResults.length === 0) {
             return <Text>No results...</Text>;
         }
 
-        return props.items.map((item) => {
+        return filteredResults.map((item) => {
             return <div key={item.id}>{item.render}</div>;
         });
     }
 
     return (
-        <Box borderLeft="1px" pl={4}>
-            <div>
-                <Input variant="filled" placeholder="Search by name..." />
-            </div>
-            <Stack spacing={2} mt={2}>
-                {renderItems()}
-            </Stack>
-        </Box>
+        <Stack direction="row" h="full">
+            <Divider orientation="vertical" />
+            <Box pl={2}>
+                <div>
+                    <Input
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        variant="filled"
+                        placeholder="Search by name..."
+                    />
+                </div>
+                <Stack spacing={2} mt={2}>
+                    {renderItems()}
+                </Stack>
+            </Box>
+        </Stack>
     );
 };
 
