1706d6ec65c1105a01fd29ad402ca560611216ec
pumbas600 <joshjeffers600@gmail.com>
2022-07-14 13:17:19 +1200

    #3 Add backing config for spawns and curse spawns

--- frontend/src/pages/GeneralConfig.tsx
+++ frontend/src/pages/GeneralConfig.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from 'react';
 import { Box, Button, Checkbox, Container, FormLabel, Radio, RadioGroup, Stack, Text } from '@chakra-ui/react';
-import Configuration from '../types/Configuration';
+import Configuration, { CurseSpawnType, SpawnType } from '../types/Configuration';
 import { invoke } from '@tauri-apps/api';
 import { convertKeysToSnakeCase } from '../utility/Utils';
 import useConfigContext from '../context/ConfigContext';
@@ -8,14 +8,6 @@ import useConfigContext from '../context/ConfigContext';
 const GeneralConfig: FC = () => {
     const [response, setResponse] = useState<string>('');
     const [config, setConfig] = useConfigContext();
-
-    function updateConfig(field: keyof Configuration, e: React.ChangeEvent<HTMLInputElement>) {
-        setConfig((config) => {
-            const updatedConfig = { ...config };
-            updatedConfig[field] = e.target.checked;
-            return updatedConfig;
-        });
-    }
 
     async function submitConfig() {
         const res = await invoke<string>('accept_config', { config: convertKeysToSnakeCase(config) });
@@ -26,20 +18,26 @@ const GeneralConfig: FC = () => {
         <Stack spacing={4}>
             <div>
                 <FormLabel>Weapon spawns</FormLabel>
-                <RadioGroup defaultValue="1">
+                <RadioGroup
+                    value={config.spawns}
+                    onChange={(type) => setConfig({ ...config, spawns: type as SpawnType })}
+                >
                     <Stack direction="row" spacing={4}>
-                        <Radio value="1">Looped</Radio>
-                        <Radio value="2">Weighted</Radio>
+                        <Radio value="Looped">Looped</Radio>
+                        <Radio value="Weighted">Weighted</Radio>
                     </Stack>
                 </RadioGroup>
             </div>
             <div>
                 <FormLabel>Curse rooms spawn</FormLabel>
-                <RadioGroup defaultValue="1">
+                <RadioGroup
+                    value={config.curseSpawns}
+                    onChange={(type) => setConfig({ ...config, curseSpawns: type as CurseSpawnType })}
+                >
                     <Stack direction="row" spacing={4}>
-                        <Radio value="1">Randomly</Radio>
-                        <Radio value="2">Always</Radio>
-                        <Radio value="3">Never</Radio>
+                        <Radio value="Randomly">Randomly</Radio>
+                        <Radio value="Always">Always</Radio>
+                        <Radio value="Never">Never</Radio>
                     </Stack>
                 </RadioGroup>
             </div>
@@ -47,10 +45,16 @@ const GeneralConfig: FC = () => {
                 <FormLabel>Other options</FormLabel>
                 <Checkbox>Configure spawns per floor</Checkbox>
                 <Checkbox>Configure spawns per room</Checkbox>
-                <Checkbox isChecked={config.disableHp} onChange={(e) => updateConfig('disableHp', e)}>
+                <Checkbox
+                    isChecked={config.disableHp}
+                    onChange={(e) => setConfig({ ...config, disableHp: e.target.checked })}
+                >
                     Disable HP
                 </Checkbox>
-                <Checkbox isChecked={config.disableOtherDrops} onChange={(e) => updateConfig('disableOtherDrops', e)}>
+                <Checkbox
+                    isChecked={config.disableOtherDrops}
+                    onChange={(e) => setConfig({ ...config, disableOtherDrops: e.target.checked })}
+                >
                     Disable other drops
                 </Checkbox>
                 <Checkbox>Blank on empty</Checkbox>
