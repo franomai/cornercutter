330b6cdbbc0b3d66b4d57d19219da9e9273f4aea
pumbas600 <joshjeffers600@gmail.com>
2022-07-14 14:23:55 +1200

    #3 Add test items

---
+++ frontend/src/data/TestItems.ts
@@ -0,0 +1,24 @@
+import SearchColumnItem from '../types/SearchColumnItem';
+
+export const Items: SearchColumnItem[] = [
+    {
+        id: '1',
+        name: 'Dolly',
+        render: 'Dolly',
+    },
+    {
+        id: '2',
+        name: 'Dynamite',
+        render: 'Dynamite',
+    },
+    {
+        id: '3',
+        name: 'Jobo Sword',
+        render: 'Jobo Sword',
+    },
+    {
+        id: '4',
+        name: 'Jobo Warhammer',
+        render: 'Jobo Warhammer',
+    },
+];
