a3c06c81401f6533efa41fd2e3792bc48126f2b9
pumbas600 <joshjeffers600@gmail.com>
2022-07-14 13:17:19 +1200

    #3 Add backing config for spawns and curse spawns

--- frontend/src/types/Configuration.ts
+++ frontend/src/types/Configuration.ts
@@ -1,9 +1,24 @@
 export default interface Configuration {
     disableHp: boolean;
     disableOtherDrops: boolean;
+    spawns: SpawnType;
+    curseSpawns: CurseSpawnType;
+}
+
+export enum SpawnType {
+    Looped = 'Looped',
+    Weighted = 'Weighted',
+}
+
+export enum CurseSpawnType {
+    Randomly = 'Randomly',
+    Always = 'Always',
+    Never = 'Never',
 }
 
 export const DEFAULT_CONFIG: Configuration = {
     disableHp: false,
     disableOtherDrops: false,
+    spawns: SpawnType.Looped,
+    curseSpawns: CurseSpawnType.Randomly,
 };
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,19 +8,34 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 struct Configuration  {
     disable_hp: bool,
-    disable_other_drops: bool
+    disable_other_drops: bool,
+    spawns: SpawnType,
+    curse_spawns: CurseSpawnType,
+
+}
+
+#[derive(Debug)]
+#[derive(Deserialize)]
+enum SpawnType {
+    Looped, Weighted
+}
+
+#[derive(Debug)]
+#[derive(Deserialize)]
+enum CurseSpawnType {
+    Randomly, Always, Never
 }
 
 #[tauri::command]
 fn accept_config(config: Configuration) -> String {
-  return format!("You submitted: Disable HP: {}, Disable other drops: {}", config.disable_hp, config.disable_other_drops);
+    return format!("You submitted: spawns: {:?}, curse spawns: {:?}", config.spawns, config.curse_spawns);
 }
 
 fn main() {
-  let context = tauri::generate_context!();
-  tauri::Builder::default()
-    .menu(tauri::Menu::os_default(&context.package_info().name))
-    .invoke_handler(tauri::generate_handler![accept_config])
-    .run(context)
-    .expect("error while running tauri application");
+    let context = tauri::generate_context!();
+    tauri::Builder::default()
+        .menu(tauri::Menu::os_default(&context.package_info().name))
+        .invoke_handler(tauri::generate_handler![accept_config])
+        .run(context)
+        .expect("error while running tauri application");
 }
