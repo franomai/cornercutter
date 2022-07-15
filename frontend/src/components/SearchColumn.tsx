e0a21942f3e20ebff60844a68c9509ffdd67b709
pumbas600 <joshjeffers600@gmail.com>
2022-07-15 16:06:21 +1200

    #3 Add clear search button

--- frontend/src/components/SearchColumn.tsx
+++ frontend/src/components/SearchColumn.tsx
@@ -1,5 +1,5 @@
-import { Box, Divider, Input, Stack, Text } from '@chakra-ui/react';
-import { FC, ReactNode, useEffect, useState } from 'react';
+import { Box, CloseButton, Divider, Input, InputGroup, InputRightElement, Stack, Text } from '@chakra-ui/react';
+import { FC, ReactNode, useEffect, useRef, useState } from 'react';
 import SearchColumnItem from '../types/SearchColumnItem';
 
 interface Props {
@@ -7,17 +7,24 @@ interface Props {
 }
 
 const SearchColumn: FC<Props> = (props) => {
+    const inputRef = useRef<HTMLInputElement>(null);
     const [search, setSearch] = useState('');
     const [filteredResults, setFilteredResults] = useState(props.items);
 
     useEffect(() => {
-        const timeout = setTimeout(() => {
-            const loweredSearch = search.toLowerCase();
-            setFilteredResults(props.items.filter((item) => item.name.toLowerCase().includes(loweredSearch)));
-        }, 500); // Wait 0.5 seconds after they've finished typing to apply the filter
+        const timeout = setTimeout(filterResults, 500); // Wait 0.5 seconds after they've finished typing to apply the filter
 
         return () => clearTimeout(timeout);
     }, [search]);
+
+    function filterResults() {
+        const loweredSearch = search.toLowerCase();
+        if (search === '') {
+            setFilteredResults(props.items);
+            return;
+        }
+        setFilteredResults(props.items.filter((item) => item.name.toLowerCase().includes(loweredSearch)));
+    }
 
     function renderItems(): ReactNode {
         if (filteredResults.length === 0) {
@@ -33,18 +40,30 @@ const SearchColumn: FC<Props> = (props) => {
         });
     }
 
+    function handleClearSearch() {
+        setSearch('');
+        setFilteredResults(props.items);
+        if (inputRef.current) inputRef.current.focus();
+    }
+
     return (
         <Stack direction="row" h="full">
             <Divider orientation="vertical" />
             <Box pl={2}>
-                <div>
+                <InputGroup>
                     <Input
+                        ref={inputRef}
                         value={search}
                         onChange={(e) => setSearch(e.target.value)}
                         variant="filled"
                         placeholder="Search by name..."
                     />
-                </div>
+                    {search !== '' && (
+                        <InputRightElement>
+                            <CloseButton title="Clear search" onClick={handleClearSearch} />
+                        </InputRightElement>
+                    )}
+                </InputGroup>
                 <Stack spacing={2} mt={2}>
                     {renderItems()}
                 </Stack>
