93cedb122fc57ce1a7c894a8a6894e65eb4484d5
franomai <fsta657@aucklanduni.ac.nz>
2022-07-17 18:06:51 +1200

    #4 Add first cut for spawn overrides through Harmony

---
+++ mod/cornercutter/FloorConfig.cs
@@ -0,0 +1,61 @@
+﻿namespace cornercutter
+{
+    // Enum for floor number, this maps across to the index returned by DungeonManager.currentFloor
+    public enum Floor
+    {
+        AllFloors = 0,
+        FirstFloor = 1,
+        SecondFloor = 2,
+        ThirdFloor = 3,
+        Boss = 4,
+    }
+
+    class FloorConfig
+    {
+        public Floor Floor { get; private set; }
+        // Spawn pools used by the appropriate swappers
+
+        // StartingSkillSwapper
+        public SpawnCollection StartingSkills { get; private set; }
+        // PickupSwapper
+        public SpawnCollection ShopSkills { get; private set; }
+        public SpawnCollection PickupSkills { get; private set; }
+        // FinaleSwapper (leveraging PickupSwapper)
+        public SpawnCollection FinaleSkills { get; private set; }
+
+        public FloorConfig(Floor floor)
+        {
+            Floor = floor;
+            ConfigOptions options  = CutterConfig.GetInstance().Options;
+            bool loopSpawns = options.HasFlag(ConfigOptions.LoopSpawns);
+
+            // Config option refers to if starting skills should be given for every floor
+            // If the collection shouldn't be needed, null it out to make errors more obvious
+            bool awardPerLevel = options.HasFlag(ConfigOptions.AwardPerLevel);
+            if (floor == Floor.AllFloors || floor == Floor.FirstFloor || awardPerLevel)
+            {
+                StartingSkills = new SpawnCollection(loopSpawns);
+            }
+            else
+            {
+                StartingSkills = null;
+            }
+
+            // All floors contain a shop
+            ShopSkills = new SpawnCollection(loopSpawns);
+
+            // Boss rooms don't contain free skill rooms (normal or curse) or finales
+            if (floor != Floor.Boss)
+            {
+                PickupSkills = new SpawnCollection(loopSpawns);
+                FinaleSkills = new SpawnCollection(loopSpawns);
+
+            }
+            else
+            {
+                PickupSkills = null;
+                FinaleSkills = null;
+            }
+        }
+    }
+}
---
+++ mod/cornercutter/SpawnCollection.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cornercutter
+{
+    class SpawnCollection
+    {
+        public List<WeightedSkill> Skills { get; private set; }
+        // Boolean for whether we should wrap around when pulling this pool or treat it as a bucket to grab from
+        private readonly bool isLooping;
+        // For now, where isLooping is set, currentIndex is used, otherwise currentTotal for weight tracking
+        // Seperated for clarity
+        private int currentIndex;
+        private int currentTotal;
+        private readonly Random rng;
+
+        public SpawnCollection(bool loopSpawns)
+        {
+            Skills = new List<WeightedSkill>();
+            currentIndex = 0;
+            isLooping = loopSpawns;
+            if (isLooping)
+            {
+                // Only instantiate if needed
+                rng = new Random();
+            }
+        }
+
+        public void AddSkill(WeightedSkill skill)
+        {
+            Skills.Add(skill);
+            if (isLooping)
+            {
+                currentTotal += skill.Weight;
+            }
+        }
+
+        public void AddSkills(params WeightedSkill[] skills)
+        {
+            foreach (WeightedSkill skill in skills)
+            {
+                AddSkill(skill);
+            }
+        }
+
+        public Entity GetNextSkill()
+        {
+            if (Skills.Count == 0) return null;
+
+            Entity selectedSkill = null;
+            if (isLooping)
+            {
+                // Take the current index item
+                selectedSkill = Skills[currentIndex].Skill;
+                currentIndex = currentIndex == Skills.Count - 1 ? 0 : currentIndex + 1;
+            }
+            else
+            {
+                // Roll the dice for the item - pick a weight, then find the item in the right weight range
+                int selectedWeight = rng.Next(0, currentTotal);
+                int rollingWeight = 0;
+                foreach (WeightedSkill weightedSkill in Skills)
+                {
+                    int newWeight = rollingWeight + weightedSkill.Weight;
+                    if (newWeight > selectedWeight)
+                    {
+                        selectedSkill = weightedSkill.Skill;
+                        break;
+                    }
+                    rollingWeight = newWeight;
+                }
+            }
+            return selectedSkill;
+        }
+    }
+}
