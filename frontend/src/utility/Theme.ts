7d43e3f0fa6f2fd9292abd34afdf393680cda13c
pumbas600 <joshjeffers600@gmail.com>
2022-09-04 12:18:53 +1200

    #10 Added component styling for checkbox

--- frontend/src/utility/Theme.ts
+++ frontend/src/utility/Theme.ts
@@ -1,10 +1,35 @@
-import { extendTheme, ThemeConfig } from '@chakra-ui/react';
+import { ComponentStyleConfig, extendTheme, ThemeConfig, withDefaultColorScheme } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
     initialColorMode: 'dark',
     useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config });
+const Checkbox: ComponentStyleConfig = {
+    baseStyle: {
+        borderColor: 'green.300',
+        control: {
+            _focus: {
+                boxShadow: '0 0 0 3px var(--chakra-colors-green-300)',
+            },
+            _checked: {
+                bg: 'green.300',
+                borderColor: 'green.300',
+
+                _hover: {
+                    bg: 'green.400',
+                    borderColor: 'green.400',
+                },
+            },
+        },
+    },
+};
+
+const theme = extendTheme({
+    config,
+    components: {
+        Checkbox,
+    },
+});
 
 export default theme;
