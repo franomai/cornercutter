c9793ebf93dd87d8ad5e5a6f2605ab4747740d4d
pumbas600 <joshjeffers600@gmail.com>
2022-09-17 16:47:24 +1200

    #10 Improve performance by using useCallback

--- frontend/src/components/tabs/generalconfig/ModInformation.tsx
+++ frontend/src/components/tabs/generalconfig/ModInformation.tsx
@@ -1,7 +1,7 @@
 import { Box, Flex, IconButton, Input, Stack, Text, useOutsideClick } from '@chakra-ui/react';
 import { faArrowUpFromBracket, faCheck, faPenToSquare, faTrash, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useRef, useState } from 'react';
+import React, { ReactNode, useCallback, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setModInfo } from '../../../redux/slices/mod';
 import ModConfig from '../../../types/Configuration';
@@ -14,116 +14,126 @@ const ModInformation = ({ selectedMod }: { selectedMod: ModConfig }) => {
     const [newName, setNewName] = useState(selectedMod.info.name);
     const [newDescription, setNewDescription] = useState(selectedMod.info.description);
 
+    const handleSaveChanges = useCallback(() => {
+        if (newName !== selectedMod.info.name || newDescription !== selectedMod.info.description) {
+            dispatch(setModInfo({ name: newName, description: newDescription }));
+        }
+        setIsEditing(false);
+    }, [dispatch, newName, newDescription, selectedMod.info]);
+
     useOutsideClick({
         ref: editableRef,
         enabled: isEditing,
         handler: handleSaveChanges,
     });
 
-    function handleSaveChanges() {
-        if (newName !== selectedMod.info.name || newDescription !== selectedMod.info.description) {
-            dispatch(setModInfo({ name: newName, description: newDescription }));
-        }
-        setIsEditing(false);
-    }
+    const handleKeyPress = useCallback(
+        (e: React.KeyboardEvent) => {
+            if (isEditing && e.key === 'Enter') {
+                handleSaveChanges();
+            }
+        },
+        [isEditing, handleSaveChanges],
+    );
 
-    function handleKeyPress(e: React.KeyboardEvent) {
-        if (isEditing && e.key === 'Enter') {
-            handleSaveChanges();
-        }
-    }
-
-    function handleDiscardChanges() {
+    const handleDiscardChanges = useCallback(() => {
         setNewName(selectedMod.info.name);
         setNewDescription(selectedMod.info.description);
         setIsEditing(false);
-    }
+    }, [selectedMod.info]);
 
-    function EditableControls({ canSave }: { canSave: boolean }) {
-        return (
-            <Stack direction="row" spacing={1.5}>
-                {isEditing ? (
-                    <>
-                        {canSave && (
+    const renderEditableControls = useCallback(
+        (canSave: boolean): ReactNode => {
+            return (
+                <Stack direction="row" spacing={1.5}>
+                    {isEditing ? (
+                        <>
+                            {canSave && (
+                                <IconButton
+                                    variant="ghost"
+                                    title="Save changes"
+                                    aria-label="Save changes"
+                                    icon={<FontAwesomeIcon icon={faCheck} size="lg" />}
+                                    onClick={handleSaveChanges}
+                                />
+                            )}
                             <IconButton
                                 variant="ghost"
-                                title="Save changes"
-                                aria-label="Save changes"
-                                icon={<FontAwesomeIcon icon={faCheck} size="lg" />}
-                                onClick={handleSaveChanges}
+                                title="Discard changes"
+                                aria-label="Discard changes"
+                                icon={<FontAwesomeIcon icon={faXmark} size="lg" />}
+                                onClick={handleDiscardChanges}
                             />
-                        )}
-                        <IconButton
-                            variant="ghost"
-                            title="Discard changes"
-                            aria-label="Discard changes"
-                            icon={<FontAwesomeIcon icon={faXmark} size="lg" />}
-                            onClick={handleDiscardChanges}
-                        />
-                    </>
-                ) : (
-                    <>
-                        <IconButton
-                            variant="ghost"
-                            title="Export mod config code"
-                            aria-label="Export mod config code"
-                            icon={<FontAwesomeIcon icon={faArrowUpFromBracket} size="lg" />}
-                        />
-                        <IconButton
-                            variant="ghost"
-                            title="Edit mod config name and description"
-                            aria-label="Edit mod config name and description"
-                            icon={<FontAwesomeIcon icon={faPenToSquare} size="lg" />}
-                            onClick={() => setIsEditing(true)}
-                        />
-                        <IconButton
-                            variant="ghost"
-                            title="Delete mod"
-                            aria-label="Delete mod"
-                            icon={<FontAwesomeIcon icon={faTrash} size="lg" />}
-                        />
-                    </>
-                )}
-            </Stack>
+                        </>
+                    ) : (
+                        <>
+                            <IconButton
+                                variant="ghost"
+                                title="Export mod config code"
+                                aria-label="Export mod config code"
+                                icon={<FontAwesomeIcon icon={faArrowUpFromBracket} size="lg" />}
+                            />
+                            <IconButton
+                                variant="ghost"
+                                title="Edit mod config name and description"
+                                aria-label="Edit mod config name and description"
+                                icon={<FontAwesomeIcon icon={faPenToSquare} size="lg" />}
+                                onClick={() => setIsEditing(true)}
+                            />
+                            <IconButton
+                                variant="ghost"
+                                title="Delete mod"
+                                aria-label="Delete mod"
+                                icon={<FontAwesomeIcon icon={faTrash} size="lg" />}
+                            />
+                        </>
+                    )}
+                </Stack>
+            );
+        },
+        [isEditing, handleSaveChanges, handleDiscardChanges],
+    );
+
+    const renderName = useCallback((): ReactNode => {
+        return isEditing ? (
+            <Input
+                fontSize="3xl"
+                fontWeight="bold"
+                value={newName}
+                onChange={(e) => setNewName(e.target.value)}
+                mr={4}
+                ml="-17px"
+                mt="-3px"
+                height="50px"
+            />
+        ) : (
+            <Text fontSize="3xl" fontWeight="bold">
+                {newName}
+            </Text>
         );
-    }
+    }, [newName, isEditing]);
+
+    const renderDescription = useCallback((): ReactNode => {
+        return isEditing ? (
+            <Input
+                ml="-17px"
+                mt={-2}
+                value={newDescription}
+                onChange={(e) => setNewDescription(e.target.value)}
+                mb={-2}
+            />
+        ) : (
+            <Text mt="3px">{newDescription}</Text>
+        );
+    }, [newDescription, isEditing]);
 
     return (
         <Stack spacing={3} ref={editableRef} onKeyDown={handleKeyPress}>
             <Flex direction="row" justifyContent="space-between" alignItems="center">
-                {isEditing ? (
-                    <Input
-                        fontSize="3xl"
-                        fontWeight="bold"
-                        value={newName}
-                        onChange={(e) => setNewName(e.target.value)}
-                        pl={2}
-                        mr={4}
-                        ml="-9px"
-                        mt="-3px"
-                        height="50px"
-                    />
-                ) : (
-                    <Text fontSize="3xl" fontWeight="bold">
-                        {newName}
-                    </Text>
-                )}
-                <EditableControls canSave={newName.trim().length !== 0 && newDescription.trim().length !== 0} />
+                {renderName()}
+                {renderEditableControls(newName.trim().length !== 0 && newDescription.trim().length !== 0)}
             </Flex>
-            <Box>
-                {isEditing ? (
-                    <Input
-                        ml="-9px"
-                        mt={-2}
-                        value={newDescription}
-                        onChange={(e) => setNewDescription(e.target.value)}
-                        pl={2}
-                        mb={-2}
-                    />
-                ) : (
-                    <Text mt="3px">{newDescription}</Text>
-                )}
-            </Box>
+            <Box>{renderDescription()}</Box>
         </Stack>
     );
 };
