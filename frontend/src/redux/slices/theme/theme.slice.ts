5529658a86e6d5c15a306d64927fd2a566170518
pumbas600 <joshjeffers600@gmail.com>
2022-10-30 17:35:46 +1300

    #46 Prevent users from dragging top of UI down on trackpad

--- frontend/src/redux/slices/theme/theme.slice.ts
+++ frontend/src/redux/slices/theme/theme.slice.ts
@@ -1,1 +1,9 @@
-export interface ThemeState {}
+export interface ThemeState {
+    backgroundColour: string;
+    primaryColour: string;
+}
+
+export const initialState: ThemeState = {
+    backgroundColour: '',
+    primaryColour: '',
+};
