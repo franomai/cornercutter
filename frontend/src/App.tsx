17084a515f6a0995ad1a5e6f49299776e59430f7
pumbas600 <joshjeffers600@gmail.com>
2022-11-18 23:28:54 +1300

    #27 Track how long users have Cornercutter open

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,27 @@
+import ReactGA from 'react-ga4';
 import ModdingConfig from './components/pages/ModdingConfig';
 
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from './redux/store';
+import { TauriEvent, listen } from '@tauri-apps/api/event';
 import { loadSavedData } from './redux/slices/saving';
 
 import './App.css';
+
+export const startTime = Date.now();
+
+listen(TauriEvent.WINDOW_CLOSE_REQUESTED, () => {
+    const runningTime = Date.now() - startTime;
+    ReactGA.event({ category: 'Application', action: 'Close Cornercutter', value: runningTime });
+});
 
 function App() {
     const dispatch = useDispatch<AppDispatch>();
 
     useEffect(() => {
         dispatch(loadSavedData());
-    }, []);
+    }, [dispatch]);
 
     return <ModdingConfig />;
 }
