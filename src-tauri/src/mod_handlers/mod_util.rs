4dad0a8f19c1ec2d62c31d8ecab7f357f8d1ec91
pumbas600 <joshjeffers600@gmail.com>
2022-12-04 23:13:06 +1300

    #21 Fix invalid mod code causing Cornercutter to crash

--- src-tauri/src/mod_handlers/mod_util.rs
+++ src-tauri/src/mod_handlers/mod_util.rs
@@ -5,7 +5,12 @@ use crate::types::structs::RoomSkills;
 
 // Modified version of the decode from lib.rs
 pub fn string_to_u32_vec(config_string: &str) -> Result<VecDeque<u32>, String> {
-    let mut decoded = decode(config_string).unwrap();
+    let decodedResult = decode(config_string);
+    if decodedResult.is_err() {
+        return Err(String::from("Invalid mod code"));
+    }
+
+    let mut decoded = decodedResult.unwrap();
 
     let mut result: VecDeque<u32> = VecDeque::new();
     // Read u8 values as u32 varints
