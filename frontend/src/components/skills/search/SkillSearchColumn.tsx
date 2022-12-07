c974323e4ee970a1b3dca666ec6ce418176d1d79
pumbas600 <joshjeffers600@gmail.com>
2022-12-07 21:57:06 +1300

    #69 Make minH 0

--- frontend/src/components/skills/search/SkillSearchColumn.tsx
+++ frontend/src/components/skills/search/SkillSearchColumn.tsx
@@ -46,7 +46,7 @@ export default function SkillSearchColumn() {
         <Flex
             minW="240px"
             maxW="240px"
-            minH="calc(100% - 58%)" // 58px here is the height of the tabs
+            minH={0} // Overflow isn't applied without some minH (And 100% maxH doesn't respect parent boundaries)
             background="blackAlpha.200"
             pt={2}
             px={4}
